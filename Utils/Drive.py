a069adee67098710b60bf3444ac730406f8ea2e9
David17-17 <64057856+Dave3811@users.noreply.github.com>
2025-11-30 10:31:51 -0500

    Fix OAuth flow and add get_estimations

--- Utils/Drive.py
+++ Utils/Drive.py
@@ -1,8 +1,10 @@
+import json
 import streamlit as st
 from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-import json
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+from io import BytesIO
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
@@ -17,26 +19,25 @@ def get_drive_service():
         )
 
     if not creds:
+        oauth_config = json.loads(st.secrets["OAUTH_CLIENT_SECRET"])
+
         flow = InstalledAppFlow.from_client_config(
-            st.secrets["OAUTH_CLIENT_SECRET"],
+            oauth_config,
             SCOPES
         )
-        creds = flow.run_console()
 
+        creds = flow.run_console()
         st.session_state["google_credentials"] = json.loads(creds.to_json())
 
     return build("drive", "v3", credentials=creds)
 
 
-def upload_pdf_to_drive(pdf_bytes: bytes, filename: str, folder_id=None):
+def upload_pdf_to_drive(pdf_bytes, filename, folder_id=None):
     service = get_drive_service()
 
-    file_metadata = {"name": filename}
+    metadata = {"name": filename}
     if folder_id:
-        file_metadata["parents"] = [folder_id]
-
-    from googleapiclient.http import MediaIoBaseUpload
-    from io import BytesIO
+        metadata["parents"] = [folder_id]
 
     media = MediaIoBaseUpload(
         BytesIO(pdf_bytes),
@@ -44,14 +45,10 @@ def upload_pdf_to_drive(pdf_bytes: bytes, filename: str, folder_id=None):
         resumable=False,
     )
 
-    file = (
-        service.files()
-        .create(
-            body=file_metadata,
-            media_body=media,
-            fields="id, webViewLink"
-        )
-        .execute()
-    )
+    file = service.files().create(
+        body=metadata,
+        media_body=media,
+        fields="id, webViewLink"
+    ).execute()
 
     return file["webViewLink"]
