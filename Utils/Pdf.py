602d76f70d9f5e3e5db8e56bcf6373b59e2d69ad
David17-17 <64057856+Dave3811@users.noreply.github.com>
2025-11-30 11:46:10 -0500

    Switch to wkhtmltopdf for PDF generation

--- Utils/Pdf.py
+++ Utils/Pdf.py
@@ -1,5 +1,5 @@
+import pdfkit
 from jinja2 import Environment, FileSystemLoader
-from weasyprint import HTML
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
@@ -7,11 +7,15 @@ TEMPLATE_DIR = BASE_DIR / "templates"
 
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
+WKHTML_PATH = "/usr/bin/wkhtmltopdf"
+
+config = pdfkit.configuration(wkhtmltopdf=WKHTML_PATH)
+
 
 def generate_estimation_pdf(data: dict) -> bytes:
     template = env.get_template("estimations.html")
     html = template.render(**data)
 
-    pdf = HTML(string=html).write_pdf()
+    pdf_bytes = pdfkit.from_string(html, False, configuration=config)
 
-    return pdf
+    return pdf_bytes
