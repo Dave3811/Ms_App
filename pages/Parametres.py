73882cf1450d4e2cce22d52e9c6a70ffcefc0e62
David17-17 <64057856+Dave3811@users.noreply.github.com>
2025-12-02 05:15:31 -0500

    Update Params

--- pages/Parametres.py
+++ pages/Parametres.py
@@ -13,3 +13,4 @@ if st.sidebar.button("🚪 Déconnexion"):
     st.session_state["username"] = None
     st.rerun()
 st.title("Gestion des paramètres")
+st.write("On va voir si tu vas vrm voir. Tu m'écriras qu'est-ce qu'on peut mettre dans paramètre quand tu vas voir ce message xD")
