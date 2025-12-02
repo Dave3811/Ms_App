8a271adcde256dee3efd243e5cc384dea37e92f7
David17-17 <64057856+Dave3811@users.noreply.github.com>
2025-12-02 05:22:03 -0500

    Updated logo

--- Utils/html_invoice.py
+++ Utils/html_invoice.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 import tempfile
+import base64
+
+
+def embed_image_base64(img_path):
+    img_file = Path(img_path)
+
+    if not img_file.exists():
+        return ""
+
+    encoded = base64.b64encode(img_file.read_bytes()).decode("utf-8")
+    return f"data:image/png;base64,{encoded}"
 
 
 def generate_invoice_html(template_path, replacements):
 
-    # Racine du projet = le dossier où se trouve app.py
     project_root = Path.cwd()
-
     template_file = project_root / template_path
 
     if not template_file.exists():
@@ -14,6 +23,14 @@ def generate_invoice_html(template_path, replacements):
 
     html = template_file.read_text(encoding="utf-8")
 
+    # --- EMBED LOGO ---
+    logo_base64 = embed_image_base64("assets/logo.png")
+    html = html.replace(
+        '<img src="assets/logo.png" alt="Logo entreprise">',
+        f'<img src="{logo_base64}" alt="Logo entreprise">'
+    )
+
+    # --- REMPLACES VARIABLES ---
     for key, val in replacements.items():
         html = html.replace(f"{{{{{key}}}}}", str(val))
 
